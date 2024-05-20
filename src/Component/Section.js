33152978372abb0bfd66d9071d56528f2d830a3f
lonelywolf757 <lonelywolf757@gmail.com>
2024-05-20 15:45:50 +0530

    Fixed Navbar placeholder text, updated Section component and CSS for album cards

--- src/Component/Section.js
+++ src/Component/Section.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import CardComponent from './CardComponent';
-import Carousel from './Carousel'; // Import the Carousel component
+import Carousel from './Carousel';
 import './Section.css';
 
 function Section({ title, apiEndpoint }) {
