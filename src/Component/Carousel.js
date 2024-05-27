79c5e770892a49b472f46480e68c1037711ac2ed
lonelywolf757 <lonelywolf757@gmail.com>
2024-05-27 16:21:32 +0530

    Configure Swiper to move multiple slides per click

--- src/Component/Carousel.js
+++ src/Component/Carousel.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef } from 'react';
-import { Swiper, SwiperSlide, useSwiper } from 'swiper/react';
+import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation } from 'swiper/modules';
 import 'swiper/swiper-bundle.css';
 import 'swiper/css/navigation';
@@ -8,11 +8,10 @@ import CarouselLeft from './CarouselLeftNavigation';
 import CarouselRight from './CarouselRightNavigation';
 
 const Controls = ({ data, swiperRef }) => {
-  const swiper = useSwiper();
-
   useEffect(() => {
     if (swiperRef.current) {
       swiperRef.current.slideTo(0);
+      console.log('Swiper instance:', swiperRef.current);
     }
   }, [data, swiperRef]);
 
@@ -23,13 +22,29 @@ const Carousel = ({ data, renderCardComponent }) => {
   const swiperRef = useRef(null);
 
   const sliderSettings = {
-    240: { slidesPerView: 2 },
-    376: { slidesPerView: 2.25, spaceBetween: 10 },
-    440: { slidesPerView: 3 },
-    680: { slidesPerView: 3 },
-    767: { slidesPerView: 4.5 },
-    1025: { slidesPerView: 6 },
-    1200: { slidesPerView: 7 },
+    240: { slidesPerView: 2, slidesPerGroup: 2 },
+    376: { slidesPerView: 2.25, spaceBetween: 10, slidesPerGroup: 2 },
+    440: { slidesPerView: 3, slidesPerGroup: 3 },
+    680: { slidesPerView: 3, slidesPerGroup: 3 },
+    767: { slidesPerView: 4.5, slidesPerGroup: 4 },
+    1025: { slidesPerView: 6, slidesPerGroup: 6 },
+    1200: { slidesPerView: 7, slidesPerGroup: 7 },
+  };
+
+  useEffect(() => {
+    if (swiperRef.current) {
+      console.log('Initial Slide Count:', swiperRef.current.slides.length);
+    }
+  }, [data]);
+
+  const verifyVisibility = () => {
+    if (swiperRef.current) {
+      const slides = swiperRef.current.slides;
+      console.log('Slide Visibility Check:');
+      for (let i = 0; i < slides.length; i++) {
+        console.log(`Slide ${i + 1} visibility:`, slides[i].isVisible);
+      }
+    }
   };
 
   return (
@@ -38,6 +53,7 @@ const Carousel = ({ data, renderCardComponent }) => {
         modules={[Navigation]}
         breakpoints={sliderSettings}
         slidesPerView={7}
+        slidesPerGroup={7} // Ensure the group setting here matches the largest value
         spaceBetween={20}
         navigation={{
           nextEl: '.arrow-right',
@@ -47,6 +63,7 @@ const Carousel = ({ data, renderCardComponent }) => {
           swiperRef.current = swiper;
           console.log('Swiper initialized:', swiper);
         }}
+        onSlideChange={() => verifyVisibility()}
       >
         <Controls data={data} swiperRef={swiperRef} />
         {data.map((item) => (
